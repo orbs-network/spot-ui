b7718417445029f5d4074f880e265330e0cd29f9
denis-orbs <101042147+denis-orbs@users.noreply.github.com>
2026-01-25 12:01:46 +0200

    a

--- packages/spot-ui/src/lib/orders/v2-orders.ts
+++ packages/spot-ui/src/lib/orders/v2-orders.ts
@@ -7,7 +7,11 @@ import BN from "bignumber.js";
 const getOrderType = (order: OrderV2) => {
   const isLimit = BN(order.order.witness.output.limit || 0).gt(1);
   const isTriggerPrice = BN(order.order.witness.output.stop || 0).gt(0);
-  const chunks = order.metadata.chunks?.length || BN(order.order.witness.input.maxAmount).div(BN(order.order.witness.input.amount)).toNumber();
+  const chunks =
+    order.metadata.chunks?.length ||
+    BN(order.order.witness.input.maxAmount)
+      .div(BN(order.order.witness.input.amount))
+      .toNumber();
 
   if (isTriggerPrice && isLimit) {
     return OrderType.TRIGGER_PRICE_LIMIT;
@@ -32,7 +36,9 @@ const getOrderType = (order: OrderV2) => {
 };
 
 const getProgress = (order: OrderV2) => {
-  const successChunks = order.metadata.chunks?.filter((chunk) => chunk.status === "success").length || 0;
+  const successChunks =
+    order.metadata.chunks?.filter((chunk) => chunk.status === "success")
+      .length || 0;
   const totalChunks = order.metadata.expectedChunks || 0;
   const progress = BN(successChunks).dividedBy(totalChunks).toNumber();
 
@@ -43,38 +49,52 @@ const getProgress = (order: OrderV2) => {
 };
 
 const getStatus = (order: OrderV2, progress: number) => {
-  if (order.metadata.status === "completed" || progress >= 99) return OrderStatus.Completed;
-  if (["pending", "eligible"].includes(order.metadata.status)) return OrderStatus.Open;
-  if (order.metadata.description.toLowerCase() === "cancelled by contract") return OrderStatus.Canceled;
+  if (order.metadata.status === "completed" || progress >= 99)
+    return OrderStatus.Completed;
+  if (["pending", "eligible"].includes(order.metadata.status))
+    return OrderStatus.Open;
+  if (order.metadata.description.toLowerCase() === "cancelled by contract")
+    return OrderStatus.Canceled;
 
   return OrderStatus.Expired;
 };
 
-
 const getFills = (order: OrderV2): OrderFill[] => {
-  const chunks = order.metadata.chunks?.filter((chunk) => chunk.status === "success") || [];
+  const chunks =
+    order.metadata.chunks?.filter((chunk) => chunk.status === "success") || [];
   return chunks.map((chunk) => ({
     inAmount: chunk.inAmount,
     outAmount: chunk.outAmount,
     timestamp: new Date(chunk.timestamp).getTime(),
     txHash: chunk.txHash,
   }));
-}
+};
 
+const getFilledOrderTimestamp = (
+  fills: OrderFill[],
+  totalTradesAmount: number,
+) => {
+  const totalFilled = fills.length;
+  if (totalFilled >= totalTradesAmount) {
+    return fills[totalFilled - 1]?.timestamp || 0;
+  }
+  return 0;
+};
 
-const getFilledOrderTimestamp = (fills: OrderFill[], totalTradesAmount: number) => {
-const totalFilled = fills.length;
-if (totalFilled >= totalTradesAmount) {
-  return fills[totalFilled - 1]?.timestamp || 0;
-}
-return 0;
-}
+const getOrderDollarValueIn = (order: OrderV2) => {
+  return BN(order.metadata.displayOnlyInputTokenPriceUSD)
+    .dividedBy(1e18)
+    .toFixed();
+};
 
 export const buildV2Order = (order: OrderV2): Order => {
   const progress = getProgress(order);
-  
+  console.log(getOrderDollarValueIn(order));
 
-  const dstMinAmountPerTrade = Number(order.order.witness.output.limit) === 1 ? "" : order.order.witness.output.limit;
+  const dstMinAmountPerTrade =
+    Number(order.order.witness.output.limit) === 1
+      ? ""
+      : order.order.witness.output.limit;
   const totalTradesAmount = order.metadata.expectedChunks || 1;
   const fills = getFills(order);
   return {
@@ -84,19 +104,29 @@ export const buildV2Order = (order: OrderV2): Order => {
     type: getOrderType(order),
     maker: order.order.witness.swapper,
     progress,
-    srcAmountFilled: fills.reduce((acc, fill) => acc.plus(fill.inAmount), new BN(0)).toFixed() || "",
-    dstAmountFilled: fills.reduce((acc, fill) => acc.plus(fill.outAmount), new BN(0)).toFixed() || "",
+    srcAmountFilled:
+      fills
+        .reduce((acc, fill) => acc.plus(fill.inAmount), new BN(0))
+        .toFixed() || "",
+    dstAmountFilled:
+      fills
+        .reduce((acc, fill) => acc.plus(fill.outAmount), new BN(0))
+        .toFixed() || "",
     fills,
     srcTokenAddress: order.order.witness.input.token,
     dstTokenAddress: order.order.witness.output.token,
-    orderDollarValueIn: order.metadata.displayOnlyInputTokenPriceUSD ? BN(order.metadata.displayOnlyInputTokenPriceUSD).toFixed(6) : "",
+    orderDollarValueIn: getOrderDollarValueIn(order),
     fillDelay: order.order.witness.epoch * 1000,
     deadline: Number(order.order.deadline) * 1000,
     createdAt: new Date(order.timestamp).getTime(),
     srcAmount: order.order.witness.input.maxAmount,
     dstMinAmountPerTrade,
-    triggerPricePerTrade: BN(order.order.witness.output.stop || 0).isZero() ? "" : order.order.witness.output.stop,
-    dstMinAmountTotal: BN(dstMinAmountPerTrade).multipliedBy(totalTradesAmount).toString(),
+    triggerPricePerTrade: BN(order.order.witness.output.stop || 0).isZero()
+      ? ""
+      : order.order.witness.output.stop,
+    dstMinAmountTotal: BN(dstMinAmountPerTrade)
+      .multipliedBy(totalTradesAmount)
+      .toString(),
     srcAmountPerTrade: order.order.witness.input.amount,
     totalTradesAmount,
     isMarketPrice: BN(dstMinAmountPerTrade || 0).lte(1),
@@ -107,11 +137,22 @@ export const buildV2Order = (order: OrderV2): Order => {
   };
 };
 
-export const getOrders = async ({ chainId, signal, account }: { chainId: number; signal?: AbortSignal; account?: string }): Promise<Order[]> => {
+export const getOrders = async ({
+  chainId,
+  signal,
+  account,
+}: {
+  chainId: number;
+  signal?: AbortSignal;
+  account?: string;
+}): Promise<Order[]> => {
   if (!account) return [];
-  const response = await fetch(`${getApiEndpoint()}/orders?swapper=${account}&chainId=${chainId}`, {
-    signal,
-  });
+  const response = await fetch(
+    `${getApiEndpoint()}/orders?swapper=${account}&chainId=${chainId}`,
+    {
+      signal,
+    },
+  );
 
   const payload = (await response.json()) as { orders: OrderV2[] };
 
