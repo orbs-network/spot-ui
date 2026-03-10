0dd11828a14d356440feb2e081b626d277bdeafa
denis-orbs <101042147+denis-orbs@users.noreply.github.com>
2026-03-10 17:17:10 +0200

    a

--- packages/spot-ui/src/lib/consts.ts
+++ packages/spot-ui/src/lib/consts.ts
@@ -83,6 +83,7 @@ export const THE_GRAPH_ORDERS_API = {
     .id]: getGfURL("berachain"),
   [networks.flare.id]: getGfURL("flare"),
   [networks.cronosZkEvm.id]: getGfURL("cronos-zkevm"),
+  
 };
 
 export const zeroAddress = "0x0000000000000000000000000000000000000000";
